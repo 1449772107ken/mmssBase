cb0ceb333778d5e9a0a50a557cf2abc6035e65f6
刘一定 <ken_account@163.com>
2022-07-03 00:24:01 +0800

    标题： mmssBase 0001 修改内容： 添加一些基础类

---
+++ MMSS_WEB_base/src/main/java/com/mmss/App.java
@@ -0,0 +1,13 @@
+package com.mmss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
